5bec39604ef6f99cbfd82841d9711b4a200b9dd5
Wasif Khan <m.wasif9905@gmail.com>
2026-05-15 15:07:49 +0500

    initial commit

---
+++ test1.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+#options = webdriver.ChromeOptions()
+#options.add_experimental_option("detach", True)
+
+# Open Chrome browser
+#driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
+
+# Open website
+driver.get("https://www.google.com")
+
+# Get page title
+title = driver.title
+
+# Verify title
+if "Google" in title:
+    print("Test Passed")
+else:
+    print("Test Failed")
+input("Test complete. Press Enter to close browser...")    
+
+
+# Close browser
+#driver.quit()
